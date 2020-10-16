5bb922dffeebae61962ddfebefcfecc2ca8f30e9
Priyank Sharma <priyank.sharma@comviva.com>
2020-10-16 17:02:15 +0530

    added Jenkinsfile

--- src/main/java/com/example/demo/CrudApp31Application.java
+++ src/main/java/com/example/demo/CrudApp31Application.java
@@ -1,18 +1,22 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class CrudApp31Application extends SpringBootServletInitializer {
 
-	
+	@Autowired
+	private static Environment env; 
 	
 	public static void main(String[] args) {
 		
 		SpringApplication.run(CrudApp31Application.class, args);
+		System.out.println(env.getProperty("server.port")+"LLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLl");
 		
 	}
 	
